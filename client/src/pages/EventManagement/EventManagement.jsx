f9e94a0ae488108dc21097080b99292d300c90a6
Akshat Sharan <akshat.sharan2004@gmail.com>
2024-06-20 00:45:09 +0530

    Export as excel fixed

--- client/src/pages/EventManagement/EventManagement.jsx
+++ client/src/pages/EventManagement/EventManagement.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../../axiosinstance";
 import Loader from "../../components/Loader/Loader";
 import { Switch } from '@mui/material';
-import { useTable, useSortBy, useGlobalFilter, usePagination } from 'react-table';
-import './eventmanagement.css';
+import * as XLSX from 'xlsx';
 import SortUpDown from '../../assets/SortUpDown.svg';
-import SortUp from '../../assets/SortUp.svg';
-import SortDown from '../../assets/SortDown.svg';
-import * as XLSX from 'xlsx'; // Import all functions from xlsx library
+import './eventmanagement.css';
 
 function EventManagement() {
     const { eventId } = useParams();
@@ -16,8 +13,9 @@ function EventManagement() {
     const [isLoading, setIsLoading] = useState(true);
     const [registrationsOpen, setRegistrationsOpen] = useState(null);
     const [registrationsData, setRegistrationsData] = useState([]);
-    const [globalFilter, setGlobalFilter] = useState('');
-    const [pageSize, setPageSize] = useState(30);
+    const [sortedData, setSortedData] = useState([]);
+    const [sortConfig, setSortConfig] = useState({ key: '', direction: '' });
+    const [searchQuery, setSearchQuery] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -27,6 +25,7 @@ function EventManagement() {
                 setEvent(eventResponse.data);
                 setRegistrationsOpen(eventResponse.data.registrationsOpen);
                 setRegistrationsData(eventResponse.data.registrations);
+                setSortedData(eventResponse.data.registrations); // Initialize sorted data
             } catch (error) {
                 console.error('Error fetching event:', error);
             } finally {
@@ -44,125 +43,80 @@ function EventManagement() {
         } catch (error) {
             console.error('Error updating event:', error);
         }
-    };
+    }
 
     const handleExportToExcel = () => {
-        try {
-            const workbook = XLSX.utils.book_new();
-            const sheetData = [];
+        const worksheetData = sortedData.map(registration => {
+            const registrationData = {
+                Registration: registration.student.collegeRegistration,
+                Name: `${registration.student.fname} ${registration.student.lname}`,
+                Phone: registration.student.phone
+            };
 
-            // Adding header row
-            const headers = [];
-            headerGroups.forEach(headerGroup => {
-                headerGroup.headers.forEach(column => {
-                    headers.push(column.Header);
-                });
-            });
-            sheetData.push(headers);
-
-            // Adding data rows
-            page.forEach(row => {
-                const rowData = [];
-                row.cells.forEach(cell => {
-                    if (cell.column.id === 'attended') {
-
-                        const selectElement = document.createElement('select');
-                        selectElement.innerHTML = cell.render('Cell');
-                        console.log(selectElement.value)
-                        rowData.push(selectElement.value);
-                    } else if (typeof cell.value === 'object') {
-                        // If cell.value is an object, stringify it or format as needed
-                        rowData.push(JSON.stringify(cell.value)); // Example: JSON.stringify(cell.value)
-                    } else {
-                        rowData.push(cell.value); // Assuming cell.value is a string or number
-                    }
-                });
-                sheetData.push(rowData);
+            event.registrationForm[0].questions.forEach(question => {
+                const answer = registration.responses.find(response => response.question === question._id)?.answer || '-';
+                registrationData[question.question] = Array.isArray(answer) ? answer.join(', ') : answer;
             });
 
-            const worksheet = XLSX.utils.aoa_to_sheet(sheetData);
-            XLSX.utils.book_append_sheet(workbook, worksheet, 'Registrations');
-            XLSX.writeFile(workbook, 'registrations.xlsx');
-        } catch (error) {
-            console.error('Error exporting to Excel:', error);
-        }
-    };
+            registrationData.Attended = registration.attended ? 'Yes' : 'No';
+            return registrationData;
+        });
+
+        const worksheet = XLSX.utils.json_to_sheet(worksheetData);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Registrations");
+
+        XLSX.writeFile(workbook, `${event.eventTitle}_registrations.xlsx`);
+    }
 
     const handelEditFormClick = () => {
         navigate(`/registration-form/${eventId}`);
+    }
+
+    const formatAnswers = (answers) => {
+        return Array.isArray(answers) ? answers.join(', ') : answers;
     };
 
-    const data = useMemo(() => registrationsData.map(registration => ({
-        ...registration,
-        collegeRegistration: registration.student.collegeRegistration,
-        attended: registration.attended,
-        phone: registration.student.phone,
-    })), [registrationsData]);
+    const handleSort = (key) => {
+        let direction = 'asc';
+        if (sortConfig.key === key && sortConfig.direction === 'asc') {
+            direction = 'desc';
+        }
 
-    const columns = useMemo(() => [
-        {
-            Header: 'Registration',
-            accessor: 'collegeRegistration',
-            canSort: true,
-        },
-        {
-            Header: 'Name',
-            accessor: row => `${row.student.fname} ${row.student.lname}`,
-            canSort: true,
-        },
-        {
-            Header: 'Phone',
-            accessor: 'phone',
-            canSort: true,
-        },
-        ...(event?.registrationForm?.flatMap(form =>
-            form.questions.map(question => ({
-                Header: question.question,
-                accessor: row => {
-                    const response = row.responses.find(response => response.question === question._id);
-                    return response ? response.answer : '-';
-                },
-                canSort: true,
-            }))
-        ) || []),
-        {
-            Header: 'Attended',
-            accessor: 'attended',
-            Cell: ({ value }) => (
-                <select defaultValue={value || '-'} className="question-type-select">
-                    <option value="" disabled>Select</option>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                </select>
-            ),
-            canSort: true,
-        },
-    ], [event]);
+        const sortedArray = [...sortedData].sort((a, b) => {
+            const getValue = (obj, key) => key.split('.').reduce((o, i) => o[i], obj);
+            const valueA = getValue(a, key);
+            const valueB = getValue(b, key);
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        page,
-        prepareRow,
-        state: { pageIndex, pageCount, canPreviousPage, canNextPage },
-        setGlobalFilter: setTableGlobalFilter,
-        nextPage,
-        previousPage,
-    } = useTable(
-        {
-            columns,
-            data,
-            initialState: { pageIndex: 0, pageSize },
-        },
-        useGlobalFilter,
-        useSortBy,
-        usePagination
-    );
+            if (valueA < valueB) {
+                return direction === 'asc' ? -1 : 1;
+            }
+            if (valueA > valueB) {
+                return direction === 'asc' ? 1 : -1;
+            }
+            return 0;
+        });
 
-    useEffect(() => {
-        setTableGlobalFilter(globalFilter);
-    }, [globalFilter, setTableGlobalFilter]);
+        setSortedData(sortedArray);
+        setSortConfig({ key, direction });
+    };
+
+    const handleSearch = (e) => {
+        const query = e.target.value.toLowerCase();
+        setSearchQuery(query);
+        const filteredData = registrationsData.filter(registration =>
+            registration.student.collegeRegistration.toLowerCase().includes(query)
+        );
+        setSortedData(filteredData);
+    };
+
+    const handleAttendanceChange = (registrationId, value) => {
+        setSortedData(prevData =>
+            prevData.map(registration =>
+                registration._id === registrationId ? { ...registration, attended: value } : registration
+            )
+        );
+    };
 
     if (isLoading) {
         return <Loader message='Fetching event' />;
@@ -187,71 +141,86 @@ function EventManagement() {
 
             <section className="event-registration-form">
                 <h2 className="management-section-header">Registrations</h2>
-                <div className="table-container">
-                    <input
-                        type="text"
-                        placeholder="Search registrations..."
-                        value={globalFilter}
-                        onChange={e => setGlobalFilter(e.target.value)}
-                    />
-                    <table className="registration-table" {...getTableProps()}>
-                        <thead>
-                            {headerGroups.map(headerGroup => (
-                                <tr {...headerGroup.getHeaderGroupProps()}>
-                                    {headerGroup.headers.map(column => (
-                                        <th key={column.id} {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                            <div className="header-items">
-                                                {column.render('Header')}
-                                                {column.isSorted
-                                                    ? column.isSortedDesc
-                                                        ? <img src={SortDown} alt="Sort Descending" className="sorting-icon" />
-                                                        : <img src={SortUp} alt="Sort Ascending" className="sorting-icon" />
-                                                    : <img src={SortUpDown} alt="Sort Descending" className="sorting-icon" />}
-                                            </div>
+                <input
+                    type="text"
+                    placeholder="Search by registration number"
+                    value={searchQuery}
+                    onChange={handleSearch}
+                    className="search-input"
+                />
+                <button onClick={handleExportToExcel} className="export-button">Export to Excel</button>
+                {event.registrationForm.map((form, index) => (
+                    <div key={index} className="table-container">
+                        <table className="registration-table">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <div className="header-items">
+                                            Registration
+                                            <button className="sorting-icon" onClick={() => handleSort('student.collegeRegistration')}>
+                                                <img className="sorting-icon" src={SortUpDown} alt="Sort" />
+                                            </button>
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div className="header-items">
+                                            Name
+                                            <button className="sorting-icon" onClick={() => handleSort('student.fname')}>
+                                                <img className="sorting-icon" src={SortUpDown} alt="Sort" />
+                                            </button>
+                                        </div>
+                                    </th>
+                                    <th>
+                                        <div className="header-items">
+                                            Phone
+                                            <button onClick={() => handleSort('student.phone')}>
+                                                <img className="sorting-icon" src={SortUpDown} alt="Sort" />
+                                            </button>
+                                        </div>
+                                    </th>
+                                    {form.questions.map((question, qIndex) => (
+                                        <th key={qIndex}>
+                                            {question.question}
                                         </th>
                                     ))}
+                                    <th>Attended</th>
                                 </tr>
-                            ))}
-                        </thead>
-                        <tbody {...getTableBodyProps()}>
-                            {page.map(row => {
-                                prepareRow(row);
-                                return (
-                                    <tr key={row.id} {...row.getRowProps()}>
-                                        {row.cells.map(cell => {
-                                            return (
-                                                <td key={cell.id} {...cell.getCellProps()}>
-                                                    {cell.render('Cell')}
+                            </thead>
+                            <tbody>
+                                {sortedData.length > 0 ?
+                                    sortedData.map((registration) => (
+                                        <tr key={registration._id}>
+                                            <td>{registration.student.collegeRegistration}</td>
+                                            <td>{registration.student.fname} {registration.student.lname}</td>
+                                            <td>{registration.student.phone}</td>
+                                            {form.questions.map((question, qIndex) => (
+                                                <td key={qIndex} className="wrapped-cell">
+                                                    {formatAnswers(registration.responses.find(response => response.question === question._id)?.answer || '-')}
                                                 </td>
-                                            );
-                                        })}
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-
-                    <div className="pagination">
-                        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                            Previous
-                        </button>
-                        <span>
-                            Page{' '}
-                            <strong>
-                                {pageIndex + 1} of {Math.ceil(data.length / pageSize)}
-                            </strong>{' '}
-                        </span>
-                        <button onClick={() => nextPage()} disabled={!canNextPage}>
-                            Next
-                        </button>
+                                            ))}
+                                            <td>
+                                                <select
+                                                    defaultValue={registration.attended || ''}
+                                                    className="question-type-select"
+                                                    onChange={(e) => handleAttendanceChange(registration._id, e.target.value)}
+                                                >
+                                                    <option value="" disabled>Select</option>
+                                                    <option value="yes">Yes</option>
+                                                    <option value="no">No</option>
+                                                </select>
+                                            </td>
+                                        </tr>
+                                    ))
+                                    :
+                                    (<tr><td colSpan={form.questions.length + 4}>No registrations found.</td></tr>)
+                                }
+                            </tbody>
+                        </table>
                     </div>
-
-                    <button onClick={handleExportToExcel}>Export as Excel</button>
-                </div>
+                ))}
             </section>
         </div>
     );
-
 }
 
 export default EventManagement;
