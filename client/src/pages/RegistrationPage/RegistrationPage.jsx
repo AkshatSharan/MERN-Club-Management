417caa0416f676ef395fecb8fe6cc391c9d50fe9
Akshat Sharan <akshat.sharan2004@gmail.com>
2024-06-19 13:30:12 +0530

    registration form styling

--- client/src/pages/RegistrationPage/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import './registrationpage.css'
 import axiosInstance from '../../axiosinstance';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -10,7 +11,8 @@ function RegistrationPage() {
     const [responses, setResponses] = useState([]);
     const { currentUser } = useSelector((state) => state.user);
     const [isLoading, setIsLoading] = useState(true);
-    const [isRegistered, setIsRegistered] = useState(false); // Added state for debugging
+
+    const textareaRef = useRef(null)
 
     const navigate = useNavigate();
 
@@ -72,6 +74,14 @@ function RegistrationPage() {
         const updatedResponses = [...responses];
         updatedResponses[index].answer = answer;
         setResponses(updatedResponses);
+        adjustTextareaHeight();
+    };
+
+    const adjustTextareaHeight = () => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto';
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
     };
 
     const handleSubmitForm = async (e) => {
@@ -91,28 +101,33 @@ function RegistrationPage() {
 
     return (
         <div>
-            <h1>{form.formTitle}</h1>
-            <p>{form.formDescription}</p>
-            <form onSubmit={handleSubmitForm}>
+            <h1 className='reg-form-title'>{form.formTitle}</h1>
+            <p className='reg-form-description'>{form.formDescription}</p>
+            <form onSubmit={handleSubmitForm} className='registration-form'>
                 {form.questions.map((question, index) => (
-                    <div key={question._id}>
-                        <h3>{question.question}</h3>
+                    <div key={question._id} className='reg-question-container'>
+                        <h3 className='reg-question'>Q{index+1}{`)`} {question.question}</h3>
                         {question.type === 'text' && (
                             <input
                                 type="text"
                                 value={responses[index].answer}
                                 onChange={(e) => handleInputChange(index, e.target.value)}
+                                className='reg-form-input'
+                                placeholder='Enter your answer'
                             />
                         )}
                         {question.type === 'textarea' && (
                             <textarea
                                 value={responses[index].answer}
                                 onChange={(e) => handleInputChange(index, e.target.value)}
+                                placeholder='Enter your answer'
+                                className='reg-textarea'
+                                ref={textareaRef}
                             />
                         )}
                         {question.type === 'radio' && (
                             question.options.map((option, optionIndex) => (
-                                <div key={optionIndex}>
+                                <div key={optionIndex} className='reg-radio'>
                                     <input
                                         type="radio"
                                         id={`${question._id}_${optionIndex}`}
@@ -127,7 +142,7 @@ function RegistrationPage() {
                         )}
                         {question.type === 'checkbox' && (
                             question.options.map((option, optionIndex) => (
-                                <div key={optionIndex}>
+                                <div key={optionIndex} className='reg-checkbox'>
                                     <input
                                         type="checkbox"
                                         id={`${question._id}_${optionIndex}`}
@@ -150,7 +165,7 @@ function RegistrationPage() {
                         )}
                         {question.type === 'select' && (
                             <select
-                                multiple={question.isMultiple} // Assume you have a field in the question object indicating if it's a multi-select
+                                multiple={question.isMultiple}
                                 value={responses[index].answer}
                                 onChange={(e) => {
                                     const options = e.target.options;
@@ -162,6 +177,7 @@ function RegistrationPage() {
                                     }
                                     handleInputChange(index, selectedValues);
                                 }}
+                                className='reg-dropdown'
                             >
                                 {question.options.map((option, optionIndex) => (
                                     <option key={optionIndex} value={option}>
@@ -172,7 +188,7 @@ function RegistrationPage() {
                         )}
                     </div>
                 ))}
-                <button type="submit">Submit</button>
+                <button type="submit" className='edit-details'>Submit</button>
             </form>
         </div>
     );
